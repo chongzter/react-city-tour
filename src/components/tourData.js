cee759c5f0b1b566579ab2d11978fcbebbbbe0fb
zackster <ftchong235@yahoo.com>
2021-07-23 23:05:28 +0800

    first commit

---
+++ src/components/tourData.js
@@ -0,0 +1,30 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "new york",
+    img: "./img/newyork.jpeg",
+    name: "new york bridge tour",
+    info: "New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean.",
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpeg",
+    name: "paris lights tour",
+    info: "Paris, France's capital, is a major European city and a global center for art, fashion, gastronomy and culture.",
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "./img/london.jpeg",
+    name: "london royal palace tour",
+    info: "London, the capital of England and the United Kingdom, is a 21st-century city with history stretching back to Roman times",
+  },
+  {
+    id: 4,
+    city: "tokyo",
+    img: "./img/tokyo.jpeg",
+    name: "tokyo sushi tour",
+    info: "Tokyo, Japan’s busy capital, mixes the ultramodern and the traditional, from neon-lit skyscrapers to historic temples.",
+  },
+];
